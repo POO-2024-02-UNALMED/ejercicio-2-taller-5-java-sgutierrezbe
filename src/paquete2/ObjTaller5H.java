16d41289df23b6a33d4ed3dda4936d7c513ab2de
Samuel Gutierrez <sgutierrezbe@unal.edu.co>
2024-12-10 12:15:31 -0500

    Preguntas

--- src/paquete2/ObjTaller5H.java
+++ src/paquete2/ObjTaller5H.java
@@ -4,7 +4,7 @@ public class ObjTaller5H {
     public static void main(String[]args){
         Moto moto = new Moto("XYZ123", "2019");
         Bus bus =new Bus("ABC345", 20);
-        moto.adelantar();
+        //moto.adelantar();
         bus.arrancar();
         bus.pitar();
         moto.getVelocidad();
